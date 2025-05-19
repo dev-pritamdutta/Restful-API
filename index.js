9c19628a3a6242fbcf7a5ae895b403e8993eade7
pritam dutta <webdevpritam7498@gmail.com>
2025-05-19 10:17:42 +0600

    basic crud layout done

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL 
+
+mongoose.connect(MONGO_URL).then(() =>{
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+}).catch((erro) => console.log(error))
