f16eca944469d7c4fd1492f1cee00a204e0748e8
pritam dutta <webdevpritam7498@gmail.com>
2025-05-19 23:55:41 +0600

    updated and deleted successfully

--- controller/userController.js
+++ controller/userController.js
@@ -30,6 +30,41 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+//update a user:
+
+export const updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findOne({ _id: id });
+
+    if (!userExist) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(201).json(updateUser);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+//delete a user:
+export const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById({ _id: id });
+    if (!userExist) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+//tesing
 export const fetch = async (req, res) => {
   try {
     return res.json("Hello world");
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,18 @@
-import { fetch, createUser, getAllUsers } from "../controller/userController.js";
+import {
+  fetch,
+  createUser,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+} from "../controller/userController.js";
 import express from "express";
-
-
 
 const route = express.Router();
 
 route.post("/create", createUser);
 route.get("/getAll", getAllUsers);
+route.put("/update/:id", updateUser);
+route.delete("/delete/:id", deleteUser);
 route.get("/fetch", fetch);
 
 export default route;
