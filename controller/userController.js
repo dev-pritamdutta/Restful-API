b8c132160d14cf4c004b85d80e01eeabc15d32af
pritam dutta <webdevpritam7498@gmail.com>
2025-05-19 14:11:09 +0600

    create the user

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,22 @@
+import User from "../model/userModel.js";
+
+export const createUser = async (req, res) => {
+  try {
+    const userData = new User(req.body);
+    const { email } = userData;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+    const savedUser = await userData.save();
+    res.status(200).json(savedUser);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 export const fetch = async (req, res) => {
   try {
     return res.json("Hello world");
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
-import { fetch } from "../controller/userController.js";
+import { fetch, createUser } from "../controller/userController.js";
 import express from "express";
 
 
 
 const route = express.Router();
 
+route.post("/create", createUser);
 route.get("/fetch", fetch);
 
 export default route;
