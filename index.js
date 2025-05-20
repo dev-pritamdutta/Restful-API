3e7ab28d1b6313fa35946fec00334e05bce4f7a4
pritam dutta <webdevpritam7498@gmail.com>
2025-05-20 21:17:51 +0600

    update

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import route from './routes/userRoute.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import route from "./routes/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGO_URL 
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(MONGO_URL).then(() =>{
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
     console.log("MongoDB connected");
 
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
-}).catch((erro) => console.log(error))
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((erro) => console.log(error));
 
 app.use("/api/user", route);
